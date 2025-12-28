9d9d7c432f89cfb4810cc2ea58b512880f42ffd9
vanductan-NLT <vanductan.nlt@gmail.com>
2025-12-28 18:57:14 +0700

    feat: update index page, i18n resources, and add vscode settings

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -77,8 +77,7 @@ export const en = {
             privacy: "We commit to keeping your information confidential.",
             success_title: "Thank you for connecting",
             success_desc: "NAM will contact you as soon as possible.",
-            reset_btn: "Fill information again",
-            join_community_btn: "Join Nam Community"
+            reset_btn: "Fill information again"
         },
         step2: {
             title: "Join the Common Home",
